8d5fc89378b888cb5d02db10e539e2c26fbb7368
Антон Новиков <avnovikov.project@ya.ru>
2022-07-01 14:45:51 +0300

    01.07.2022. Final!

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -34,8 +34,10 @@ def profile(request, username):
     author = get_object_or_404(User, username=username)
     posts_list = author.posts.all()
     page_obj = paginator(posts_list, request)
-    following = request.user.is_authenticated and Follow.objects.filter(
-        user=request.user, author=author.id).exists()
+    following = (
+        request.user.is_authenticated
+        and Follow.objects.filter(user=request.user, author=author).exists()
+    )
     template = 'posts/profile.html'
     context = {
         'author': author,
