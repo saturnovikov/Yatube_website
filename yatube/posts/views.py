d38687802c7a75a93c38e138985e40e9f6e271e2
Антон Новиков <avnovikov.project@ya.ru>
2022-07-01 14:20:04 +0300

    Отправка 01.07.2022. Версия 1.6.После третьего ревью.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -34,11 +34,8 @@ def profile(request, username):
     author = get_object_or_404(User, username=username)
     posts_list = author.posts.all()
     page_obj = paginator(posts_list, request)
-    cheking_follow = False
-    if request.user.is_authenticated:
-        cheking_follow = Follow.objects.filter(
-            user=request.user, author=author.id).exists()
-    following = cheking_follow and request.user.is_authenticated
+    following = request.user.is_authenticated and Follow.objects.filter(
+        user=request.user, author=author.id).exists()
     template = 'posts/profile.html'
     context = {
         'author': author,
